d2d013a792a355d87755e69e83715f3872801a46
vashtileonie <vashti.delapaz@gmail.com>
2023-10-26 21:29:14 +0800

    error handling for login

--- final-project/project/src/app/login/login.component.ts
+++ final-project/project/src/app/login/login.component.ts
@@ -8,11 +8,13 @@ import { DataService } from '../data.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+ 
 })
 export class LoginComponent {
   accountCheck: FormGroup;
   account : Account;
+  errorMessage: string; 
   constructor(private accountService: AccountService, private fb: FormBuilder, private router: Router, private dataService: DataService) {
     this.accountCheck = this.fb.group({
       username: '',
@@ -22,23 +24,28 @@ export class LoginComponent {
 
   onSubmit(){
     this.accountService.checkAccount(this.accountCheck.value.username, this.accountCheck.value.password).subscribe(
-      (account : Account) => {
+      (account: Account) => {
         // Handle successful response
         this.account = account;
-        if (account.role === 'ADMIN') {
-          // Redirect to admin dashboard
-          this.dataService.setDataPersistent('account', account);
-          this.router.navigate(['/dashboard']);
-        } else if (account.role === 'USER') {
-          // Redirect to user dashboard
-          this.dataService.setDataPersistent('account', account);
-          this.router.navigate(['/user-dashboard']);
+        if (account && account.role) {
+          if (account.role === 'ADMIN') {
+            // Redirect to admin dashboard
+            this.dataService.setDataPersistent('account', account);
+            this.router.navigate(['/dashboard']);
+          } else if (account.role === 'USER') {
+            // Redirect to user dashboard
+            this.dataService.setDataPersistent('account', account);
+            this.router.navigate(['/user-dashboard']);
+          }
+        } else {
+          // Handle the case when 'account' is null or 'role' is not defined
+          this.errorMessage = 'Invalid credentials. Please try again.';
         }
-        //this.router.navigate(['/dashboard']);
       },
       (error) => {
-        // Handle error
+        // Handle error and set the error message
         console.error('Error: ', error);
+        this.errorMessage = 'Login failed. Please check your username and password.';
       }
     );
 
