3e12b213a4fae9d24dbf4834ed24c89d5da0131a
vashtileonie <vashti.delapaz@gmail.com>
2024-04-07 19:13:41 +0800

    homepage(index)

--- final-project/project/src/app/index/index.component.ts
+++ final-project/project/src/app/index/index.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Account } from '../model/account';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-index',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent {
+  account : Account;
+  constructor(config: NgbCarouselConfig, private router: Router, private dataService: DataService) {
+    config.interval = 2000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+  }
 
+  navigateToHome(){
+    this.account = this.dataService.getDataPersistent('account');
+    if (this.account == null) {
+      this.router.navigate(['index']);
+    } else if (this.account.role == 'ADMIN'){
+      this.router.navigate(['/dashboard']);
+    } else if (this.account.role == 'USER'){
+      this.router.navigate(['/user-dashboard']);
+    }
+  }
 }
